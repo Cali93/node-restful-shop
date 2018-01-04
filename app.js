4bb07d5922fb5c7c5206986f161ae6f5a91d035f
Cali93 <c.armut93@outlook.com>
2018-01-04 21:01:44 +0100

    MongoDB & Mongoose are OP

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require ('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+// mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/noderestfulshop')
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
